3eb3360387871e9fd435fa080fb2360d96b0ca93
yuriiShmal <yriy.shmal@gmail.com>
2024-06-14 12:32:52 +0300

    Added transitional layouts for the process of connecting and successful disconnection to the app.

---
+++ androidfreevpn/src/main/java/com/androidfreevpn/activities/DisconnectedSummaryActivity.kt
@@ -0,0 +1,14 @@
+package com.androidfreevpn.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.androidfreevpn.R
+
+class DisconnectedSummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.connected_screen)
+
+    }
+}
